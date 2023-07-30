ebe8d0bac3f4733fe21f4774aa3c838fbffe3c25
AntonioSkara132 <conio1240@gmail.com>
2023-07-30 19:39:20 +0200

    update ImageStatisticalAnalysisUtils.py

--- code/ImageStatisticalAnalysisUtils.py
+++ code/ImageStatisticalAnalysisUtils.py
@@ -1,5 +1,4 @@
 import glob
-import os
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage import io
@@ -83,21 +82,24 @@ class GalleryAnalyzer:
         plt.hist(intensities, bins=bins, weights=self.freqs)
 
 class Stats:
-    "Calculates and stores statistical information about given data"
+    "Calculates statistical information for given data"
     data = []
     def __init__(self, data):
         self.data = data
-
-    def createHistogram(self):
+    def plotHistogram(self):
+        """plots histogram of given data"""
         _ = plt.hist(self.data, bins=256)
         plt.show()
-
     def getMi(self) -> float:
+        """calculates average value of data"""
         return np.average(self.data)
     def getSd(self) -> float:
+        """calculates standard deviation of data"""
         return np.std(self.data)
     def getMed(self) -> float:
+        """calculates median of data"""
         return np.median(self.data)
     def getData(self):
+        """returns data"""
         return self.data
 
