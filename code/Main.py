04e74fea99d7b7aa23c9ff4f27372317c39b8a0d
AntonioSkara132 <conio1240@gmail.com>
2023-07-30 23:16:45 +0200

    update

--- code/Main.py
+++ code/Main.py
@@ -13,16 +13,22 @@ def main():
         exit(-1)
 
     MyGa = isa.GalleryAnalyzer(dir_path)
-    plt.figure()
+    fig1 = plt.figure()
     MyGa.createCommonHistogram()
     plt.title("Pixel distribution")
-    plt.figure()
+    fig2 = plt.figure()
     MyGa.createMedHistogram(bins = 32)
     MyGa.createMiHistogram(bins = 32)
     MyGa.createSdHistogram(bins = 32)
     _ = plt.legend(['Median', 'Average', 'Standard Deviation'])
     plt.title("Statistical data")
     plt.show()
+
+    if len(sys.argv) > 2:
+        fig1.savefig(sys.argv[2] % "pixel_histogram")
+        fig2.savefig(sys.argv[2] % "med_mi_sd_histogram")
+    else:
+        plt.show()
 
 if __name__ == "__main__":
         main()
