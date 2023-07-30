dc6ed69c6e61ec26714bdd275c1ebd1766b64cae
AntonioSkara132 <conio1240@gmail.com>
2023-07-30 20:43:29 +0200

    Directory reorganized

--- code/Main.py
+++ code/Main.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 
 def main():
 
+    #dir_path = "data/*.jpg"
     if sys.argv[0]:
         dir_path = sys.argv[1]
     else:
         print("No directory specified!")
         exit(-1)
 
-    dir_path = "data/*.jpg"
     MyGa = isa.GalleryAnalyzer(dir_path)
     plt.figure()
     MyGa.createCommonHistogram()
